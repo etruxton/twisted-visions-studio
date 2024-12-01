b848b8bbb444d5c45fbb18f0d3baa562dce991d5
etruxton <awesomeerica743@gmail.com>
2024-12-01 17:05:12 -0600

    update contact and about me descriptions

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -13,10 +13,15 @@ const About = () => {
       <div className="about-container">
         {/* About Me Section */}
         <div className="about-me">
-          <h2>About Me</h2>
+          <h2>About the Artist</h2>
           <p>
-            Hi, I'm Rich Powers, a artist passionate about creating unique sculptures out of chicken wire.
-            [insert more description here]
+          Meet Richard Powers; he is a 100% disabled veteran with PTSD who is primarily homebound. He has found peace and purpose through his art. He prefers to keep this private and appreciates understanding regarding his wish not to discuss it extensively with others. 
+          </p>
+          <p>
+          His journey began unexpectedly in the backyard, where he picked up a stray piece of chicken wire and saw a face looking back at him. That moment sparked a creative obsession, and as you can see from his gallery, his skills and artistry have flourished over the years. 
+          </p>
+          <p>
+          He is particularly drawn to creating life-sized sculptures—typically larger than a turkey—and specializes in pieces that showcase intricate, textured "hair."
           </p>
         </div>
 
--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -6,18 +6,39 @@ const Contact = () => {
   const navigate = useNavigate(); // Hook for navigation
 
   return (
-    <div>
-      
+    <div>        
     {/* Back to Gallery Button */}
-    <button className="back-to-gallery" onClick={() => navigate('/')}>
-      &larr; Back to Gallery
-    </button>
-    <div className="contact-container">
-      <h2>Contact Us</h2>
-      <p>Email: artist@example.com</p>
-      <p>Phone: (123) 456-7890</p>
-      <p>Facebook Group: <a href="https://www.facebook.com/groups/">Group</a> </p>
-    </div>
+      <button className="back-to-gallery" onClick={() => navigate('/')}>
+        &larr; Back to Gallery
+      </button>
+      <div className="contact-container">
+        {/* contact Me Section */}
+        <div className="contact-me">
+          <h2>Contact Us</h2>
+          <p>
+          Richard does not sell his art, but he accepts gift cards to cover the cost of materials. Since each project varies in size and complexity, Visa or MasterCard gift cards work best to allow flexibility in sourcing supplies. 
+          </p>
+          <p>
+          Richard will not make or consider any art that is political, religious nor controversial in any manner and has the right to refuse service for any reason.
+          </p>
+          <p>
+          If you are interested in a piece, you can apply through Facebook Messenger communicating with his wife <b>Page Powers</b>. 
+          </p>
+          <p>
+          Page Powers Facebook: <a href="https://www.facebook.com/page.powers62">https://www.facebook.com/page.powers62</a>
+          </p>
+          <p>
+          Twisted Visions Studio Facebook Group: <u>Coming Soon</u>
+          </p>
+        </div>
+
+        {/* Image Gallery Section */}
+        <div className="contact-images">
+          <img class="image-right" src={require('../../assets/images/aboutme_artist.jpg')} alt="Artwork 1" />
+        </div>
+        <div className="contact-images">
+        </div>
+      </div>
     </div>
   );
 };
