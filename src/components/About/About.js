6fa23e883396ec610e25fbaeb0cb268ed8193c70
etruxton <awesomeerica743@gmail.com>
2024-11-30 23:27:36 -0600

    add back to gallery button and update about me and contact page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,22 +1,31 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import './About.css';
 
 const About = () => {
+  const navigate = useNavigate(); // Use React Router for navigation
   return (
-    <div className="about-container">
-      {/* About Me Section */}
-      <div className="about-me">
-        <h2>About Me</h2>
-        <p>
-          Hi! I'm [name], a artist passionate about creating unique artwork out of chicken wire.
-          [insert more description here]
-        </p>
-      </div>
+    <div>        
+    {/* Back to Gallery Button */}
+      <button className="back-to-gallery" onClick={() => navigate('/')}>
+        &larr; Back to Gallery
+      </button>
+      <div className="about-container">
+        {/* About Me Section */}
+        <div className="about-me">
+          <h2>About Me</h2>
+          <p>
+            Hi, I'm Rich Powers, a artist passionate about creating unique sculptures out of chicken wire.
+            [insert more description here]
+          </p>
+        </div>
 
-      {/* Image Gallery Section */}
-      <div className="about-images">
-        <img src={require('../../assets/images/aboutme_turkey.jpg')} alt="Artwork 1" />
-        <img src={require('../../assets/images/aboutme_artist.jpg')} alt="Artwork 2" />
+        {/* Image Gallery Section */}
+        <div className="about-images">
+          <img class="image-right" src={require('../../assets/images/aboutme_turkey.jpg')} alt="Artwork 1" />
+        </div>
+        <div className="about-images">
+        </div>
       </div>
     </div>
   );
--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom'; // Import useNavigate for navigation
+import './Contact.css'; // Import CSS for styling
 
-const Contact = () => (
-  <div className="contact">
-    <h2>Contact</h2>
-    <p>Email: artist@example.com</p>
-    <p>Phone: (123) 456-7890</p>
-  </div>
-);
+const Contact = () => {
+  const navigate = useNavigate(); // Hook for navigation
+
+  return (
+    <div>
+      
+    {/* Back to Gallery Button */}
+    <button className="back-to-gallery" onClick={() => navigate('/')}>
+      &larr; Back to Gallery
+    </button>
+    <div className="contact-container">
+      <h2>Contact Us</h2>
+      <p>Email: artist@example.com</p>
+      <p>Phone: (123) 456-7890</p>
+      <p>Facebook Group: <a href="https://www.facebook.com/groups/">Group</a> </p>
+    </div>
+    </div>
+  );
+};
 
 export default Contact;
