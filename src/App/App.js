c2c1654fe2b179ae69469b5efa163bdbbdf242ce
etruxton <awesomeerica743@gmail.com>
2025-01-25 12:00:53 -0600

    add sitemap, robots.txt, and metadata for search engines to find website

--- src/App/App.js
+++ src/App/App.js
@@ -5,11 +5,16 @@ import About from '../components/About/About';
 import Contact from '../components/Contact/Contact';
 import ImagePage from '../components/ImagePage/ImagePage'; // The page for displaying the individual image
 import NotFound from '../components/NotFound/NotFound'; // Import the NotFound component
-
+import { HelmetProvider } from 'react-helmet-async'; // Import HelmetProvider for setting the title and meta tags in Google Search results
 function App() {
   return (
     <Router>
       <div id="page-container">
+        <HelmetProvider>
+          <title>Twisted Visions Studio</title>
+          <meta name='description' content='Discover hand-crafted wire art ranging from barnyard animals to full sized mascots.' />
+          <meta name="keywords" content="chicken wire sculptures, wire art, wire sculptures, hand-crafted art, metal wire artwork, chicken wire artist, chicken wire, wire" />
+        </HelmetProvider>
       <div id="content-wrap">
         <Header />
         <Routes>
